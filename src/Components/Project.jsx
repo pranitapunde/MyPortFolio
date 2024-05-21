920ab0c2f9c17e0c62f01587ddb9f1c8674fbf72
pranitapunde <pundepranita5@gmail.com>
2024-05-21 15:33:41 +0530

    Second Commit

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -48,13 +48,12 @@ const Project = () => {
     ];
 
 
-
     return (
         <div
         id='project'
             name="portfolio"
-            className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-auto portfolio">
-            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            className="  bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-auto portfolio">
+            <div className=" project-sec max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full items-center">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">
                         Projects
@@ -62,7 +61,7 @@ const Project = () => {
                     <p className="py-6">Check out some of my work right here</p>
                 </div>
 
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+                <div className="projectCard grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
                     {porjects.map(({ id, src, link, repo }) => (
                         <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
                             <img
